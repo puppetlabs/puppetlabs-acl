cb0e4cf9df62d37c2350559dfe0ed076a7925f75
Rob Reynolds <ferventcoder@gmail.com>
2014-01-31 14:23:44 -0600

    (maint) cleanup/line ending fix for ace.rb

--- lib/puppet/type/acl/ace.rb
+++ lib/puppet/type/acl/ace.rb
@@ -12,6 +12,7 @@ class Puppet::Type::Acl::Ace
     @identity = permission_hash["identity"]
     @rights = permission_hash["rights"]
     #newvalues(:full, :modify, :write, :list, :read, :execute)
+    # binary hex flags
     @type = validate_and_return(permission_hash["type"] || "allow",:allow,:deny)
     @child_types = validate_and_return(permission_hash["child_types"] || "all",:all, :objects, :containers)
     @affects = validate_and_return(permission_hash["affects"] || "all",:all, :self_only, :children_only, :self_and_direct_children, :direct_children_only)
@@ -26,58 +27,3 @@ class Puppet::Type::Acl::Ace
   end
 
 end
-
-
-# Puppet::Type.newtype(:ace) do
-#   @doc = <<-'EOT'
-#     Manages access control lists.  The `acl` type is typically
-#     used in when you need more complex management of permissions
-#     e.g. windows.
-
-#     Sample usage:
-
-#       ADD HERE LATER
-#   EOT
-
-#   feature :ace_order_required, "The provider determines if the order of access control entries (ACE) is required."
-
-#   newparam(:name) do
-#     desc "The name of the acl resource.  Used for uniqueness."
-#     isnamevar
-#   end
-
-#   newparam(:identity) do
-#     desc "Identity is also known as a trustee or principal - what gets
-#       assigned a particular set of permissions (or access rights).
-#       This can be in the form of: 1. User - e.g. 'Bob' or 'TheNet\Bob',
-#       2. Group e.g. 'Administrators' or 'BUILTIN\Administrators', 3.
-#       SID (Security ID) e.g. 'S-1-5-18'."
-#     defaultto ''
-#   end
-
-#   newparam(:rights) do
-#     desc "[Empty for now]"
-#     #newvalues(:full, :modify, :write, :list, :read, :execute)
-
-#     # binary hex flags
-#     defaultto []
-#   end
-
-#   newparam(:type) do
-#     desc "[Empty for now]"
-#     newvalues(:allow, :deny)
-#     defaultto :allow
-#   end
-
-#   newparam(:child_types) do
-#     desc "[Empty for now]"
-#     newvalues(:all, :objects, :containers)
-#     defaultto :all
-#   end
-
-#   newparam(:affects) do
-#     desc "[Empty for now]"
-#     newvalues(:all, :self_only, :children_only, :self_and_direct_children, :direct_children_only)
-#     defaultto :all
-#   end
-# end
