e5e1eac98d2131ccb53c029d127c16f89ee88582
Rob Reynolds <ferventcoder@gmail.com>
2014-01-31 14:19:00 -0600

    (maint) heria.pp cleanup

--- tests/hiera.pp
+++ tests/hiera.pp
@@ -1,36 +1,54 @@
-$acls = hiera_hash('acls',{})
-notice $acls
-create_resources(acl,$acls)
+$acls = hiera_hash('acls', {})
+
+notify { $acls: }
+create_resources(acl, $acls)
 
 $tempAcl = Acl['tempdir']
 
-acl {'c:/temp':
-  ensure => present,
+acl { 'c:/temp':
+  ensure      => present,
   permissions => [
-    {identity => 'Administrators', rights => [full]}
+    {
+      identity => 'Administrators',
+      rights   => [full]
+    }
   ],
-  owner => 'Administrators',
+  owner       => 'Administrators',
   inherit_parent_permissions => true
 }
 
 acl { 'temp_dir_module_name':
-  target => 'c:/temp',
-  ensure => present,
+  target      => 'c:/temp',
+  ensure      => present,
   permissions => [
-    {identity => 'bob', rights => [read,execute]},
-    {identity => 'tim', rights => [read,execute]}
+    {
+      identity => 'bob',
+      rights   => [read,execute]
+    },
+    {
+      identity => 'tim',
+      rights   => [read,execute]
+    }
   ],
 }
 
 acl { 'temp_dir_module2_name':
-  target => 'c:/temp',
-  ensure => present,
+  target      => 'c:/temp',
+  ensure      => present,
   permissions => [
-    {identity => 'bill', rights => [full], affects => self_only},
-    {identity => 'tim', rights => [read,execute]}
+    {
+      identity => 'bill',
+      rights   => [full],
+      affects  => self_only
+    }
+    ,
+    {
+      identity => 'tim',
+      rights   => [read,execute]
+    }
   ],
 }
 
 $tempAcl2 = Acl['c:/temp']
 
-#$foo = inline_template("<% require 'pry';binding.pry %>")
+# $foo = inline_template("<% require 'pry';binding.pry %>")
