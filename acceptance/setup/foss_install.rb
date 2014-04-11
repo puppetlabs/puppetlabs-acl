1b7ad0af903e5627294b2320784e548cb6c01be3
Ryan Gard <ryan.gard@puppetlabs.com>
2014-04-11 14:35:05 -0700

    Add first batch of acceptance tests for ACL module.

---
+++ acceptance/setup/foss_install.rb
@@ -0,0 +1,9 @@
+test_name "Install ACL Module on Master"
+
+step "Clone Git Repo"
+on master, "git clone https://github.com/cowofevil/puppetlabs-acl.git /etc/puppet/modules/acl"
+
+step "Plug-in Sync Each Agent"
+with_puppet_running_on master, :main => { :verbose => true, :daemonize => true } do
+  on agents, puppet("plugin download --server #{master}")
+end
