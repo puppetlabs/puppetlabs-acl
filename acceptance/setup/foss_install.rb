16e2ec0fd6e582091d224a5f72a471d0da00d2df
Ryan Gard <ryan.gard@puppetlabs.com>
2014-04-16 13:36:11 -0700

    Fix incorrect Git repo for the FOSS install script

--- acceptance/setup/foss_install.rb
+++ acceptance/setup/foss_install.rb
@@ -1,7 +1,7 @@
 test_name "Install ACL Module on Master"
 
-step "Clone Git Repo"
-on master, "git clone https://github.com/cowofevil/puppetlabs-acl.git /etc/puppet/modules/acl"
+step "Clone Git Repo on Master"
+on(master, "git clone https://github.com/puppetlabs/puppetlabs-acl.git /etc/puppet/modules/acl")
 
 step "Plug-in Sync Each Agent"
 with_puppet_running_on master, :main => { :verbose => true, :daemonize => true } do
