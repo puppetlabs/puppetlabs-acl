621a9af5e7a35175fe1ca15f138a4b66fb556f42
Ethan J. Brown <Iristyle@github>
2014-04-29 11:00:39 -0700

    (PUP-2338) Guard symlink tests on platform support

--- spec/integration/provider/acl/windows_spec.rb
+++ spec/integration/provider/acl/windows_spec.rb
@@ -71,7 +71,8 @@ describe Puppet::Type.type(:acl).provider(:windows), :if => Puppet.features.micr
       resource[:target] = set_path('set_target')
     end
 
-    it "should not allow permissions to be set on directory symlinks (PUP-2338)" do
+    it "should not allow permissions to be set on directory symlinks (PUP-2338)",
+        :if => Puppet.features.manages_symlinks? do
       target_path = set_path('symlink_target')
       resource[:target] = File.expand_path("fake",resource[:target])
       Puppet::Util::Windows::File.symlink(target_path,resource[:target])
@@ -81,7 +82,8 @@ describe Puppet::Type.type(:acl).provider(:windows), :if => Puppet.features.micr
       }.to raise_error(Puppet::ResourceError, /Puppet cannot manage ACLs of symbolic links/)
     end
 
-    it "should not allow permissions to be set on file symlinks (PUP-2338)" do
+    it "should not allow permissions to be set on file symlinks (PUP-2338)",
+        :if => Puppet.features.manages_symlinks? do
       target_path = set_path('symlink_target')
       file_path = File.join(target_path,"file.txt")
       FileUtils.touch(file_path)
