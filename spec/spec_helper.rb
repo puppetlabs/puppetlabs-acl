9183df30bc51a5838dffad351b49b208c4794755
Glenn Sarti <glennsarti@users.noreply.github.com>
2018-02-08 15:27:20 +0800

    (MODULES-5899) Fix spec test helpers for rubocop violations
    This commit updates the spec test helpers to fix any rubocop violations.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,36 +7,34 @@ if Puppet.features.microsoft_windows?
   require 'puppet/util/windows/security'
 
   def take_ownership(path)
-    path = path.gsub('/', '\\')
-    output = %x(takeown.exe /F #{path} /R /A /D Y 2>&1)
-    if $? != 0 #check if the child process exited cleanly.
-      puts "#{path} got error #{output}"
-    end
+    path = path.tr('/', '\\')
+    output = `takeown.exe /F #{path} /R /A /D Y 2>&1`
+    puts "#{path} got error #{output}" if $CHILD_STATUS != 0 # check if the child process exited cleanly.
   end
 end
 
 RSpec.configure do |config|
-  tmpdir = Dir.mktmpdir("rspecrun_acl")
-  oldtmpdir = Dir.tmpdir()
+  tmpdir = Dir.mktmpdir('rspecrun_acl')
+  oldtmpdir = Dir.tmpdir
   ENV['TMPDIR'] = tmpdir
 
   config.expect_with :rspec do |c|
     c.syntax = [:should, :expect]
   end
-  
-  #config.before :each do
+
+  # config.before :each do
   #  # Disabling garbage collection inside each test, and only running it at
   #  # the end of each block, gives us an ~ 15 percent speedup, and more on
   #  # some platforms *cough* windows *cough* that are a little slower.
   #  GC.disable
-  #end
+  # end
   #
-  #config.after :each do
+  # config.after :each do
   #  # This will perform a GC between tests, but only if actually required.  We
   #  # experimented with forcing a GC run, and that was less efficient than
   #  # just letting it run all the time.
   #  GC.enable
-  #end
+  # end
 
   config.after :suite do
     # return to original tmpdir
@@ -51,6 +49,6 @@ end
 # We need this because the RAL uses 'should' as a method.  This
 # allows us the same behaviour but with a different method name.
 class Object
-  alias :must :should
-  alias :must_not :should_not
+  alias must should
+  alias must_not should_not
 end
