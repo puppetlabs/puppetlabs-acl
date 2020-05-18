3c42bc454057856f167d164c4f97ebf21c16e682
Daniel Carabas <daniel.carabas@puppet.com>
2020-05-18 10:02:01 +0300

    Switch to default spec_helper_acceptance file

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require 'serverspec'
 require 'puppet_litmus'
-require 'pry'
 require 'spec_helper_acceptance_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_acceptance_local.rb'))
 
 PuppetLitmus.configure!
